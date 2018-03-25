9cf628fdd9bccf27a7a219728e24da889ab36937
Jessica Fegial <jessica.feigal@irishtitan.com>
2018-03-25 18:03:41 -0500

    complete landing page project

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="css/styles.css" />
+
+
+    <title>Opengrain Bottle Openers & Decor</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row header">
+        <div class="col logo-image-name">
+          <div class="logo-image">
+            <a href="/"><img src="img/logo.png" alt="Opengrain Logo of green wood grain"/></a>
+          </div>
+          <div class="logo-name">
+            <a href="/"><span class="logo-name">Opengrain</span></a>
+          </div>
+        </div>
+        <div class="col-2 col-sm-3 text-right d-flex align-items-center">
+          <div class="social-media">
+            <a href="http://facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="http://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="http://pinterest.com" target="_blank"><i class="fab fa-pinterest-square"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="content">
+      <div class="procons">
+        <div class="row no-gutters dark-green-bkg brown">
+          <div class="col">
+            <img src="img/sad-3-ppl-crop_16-9.png" alt="Three bored young adults lounging around on outside patio." />
+          </div>
+          <div class="col-5 col-sm-3 d-flex align-items-center">
+            <p class="heading large">Is your summer party a Fail?</p> 
+          </div>
+        </div>
+        <div class="row no-gutters dark-green-bkg brown">
+          <div class="col-5 col-sm-3 d-flex align-items-center">
+            <p class="heading large text-right">Get the partying started with bottle openers!</p>
+          </div>
+          <div class="col">
+            <img src="img/happy-friends-crop_16_9.png" alt="Outside picnic table filled with food and drinks, surrounded by happy friends enjoying the spread." />
+          </div>
+        </div>
+      </div>
+
+      <div class="promotional">
+        <div class="row light-green section-header">
+          <div class="col text-center">
+            <p class="large">Plan Your Perfect Summer</p>
+          </div>
+        </div>
+        <div class="row products white-bkg">
+          <div class="col text-center">
+            <div class="image"><a href="#" taget="_blank"><img src="img/catcher.png" alt="bottle opener with a basket to catch caps"/></a></div>
+            <div class="title heading">Rustic Opener & Catcher</div>
+            <div class="cta"><a href="#" taget="_blank">Get Mine Today <i class="fas fa-angle-right"></i></a></div>
+          </div>
+          <div class="col text-center">
+            <div class="image"><a href="#" taget="_blank"><img src="img/nuts_&_bolts.png" alt="bottle opener with mounting hardware"/></a></div>
+            <div class="title heading">Beer Thirty Bottle Opener</div>
+            <div class="cta"><a href="#" taget="_blank">Get Mine Today <i class="fas fa-angle-right"></i></a></div>
+          </div>
+          <div class="col text-center">
+            <div class="image"><a href="#" taget="_blank"><img src="img/decor.png" alt="bottle opener cap decoration"/></a></div>
+            <div class="title heading">Personalized Dakota Opener</div>
+            <div class="cta"><a href="#" taget="_blank">Get Mine Today <i class="fas fa-angle-right"></i></a></div>
+          </div>
+        </div>
+        <div class="row cta-full">
+          <div class="col text-center">
+            <a href="#" taget="_blank">View Our Full Collection</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Created by: Jess Feigal
+Created date: 3/25/2018
+
+Purpose: Making up and example of a responsive landing page.
+Goal: To drive users to purchase products.
